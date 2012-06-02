8da808624a647ad681e0ae4f09e9a383c761f3d8
Alberto Piai <alberto.piai@gmail.com>
2012-06-02 09:06:55 +0200

    Initial Commit

---
+++ WebInspectorProxy/WIPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  WIPAppDelegate.h
+//  WebInspectorProxy
+//
+//  Created by asdasd asdasd on 6/2/12.
+//  Copyright (c) 2012 asdasd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WIPViewController;
+
+@interface WIPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) WIPViewController *viewController;
+
+@end
---
+++ WebInspectorProxy/WIPViewController.h
@@ -0,0 +1,13 @@
+//
+//  WIPViewController.h
+//  WebInspectorProxy
+//
+//  Created by asdasd asdasd on 6/2/12.
+//  Copyright (c) 2012 asdasd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WIPViewController : UIViewController
+
+@end
